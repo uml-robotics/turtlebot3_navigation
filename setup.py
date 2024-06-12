4cd540f7165daeb3d1225691465ec3475fb60b84
flynnbm <flynn.m.brian@gmail.com>
2024-06-12 09:12:21 -0400

    update dependencies for example; add sensor_msgs, also update setup.py to look for launch files and create example executable

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'turtlebot3_navigation'
 
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'example_node = turtlebot3_navigation.example:main'
         ],
     },
 )
